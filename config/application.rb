702c4e6882b5373c4e64df5066029846627f46f9
yuta-iiduka <ota12041204@gmail.com>
2021-03-09 07:08:11 +0000

    install-devise

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-<<<<<<< HEAD
 module Taskshare
-=======
-module TaskShare
->>>>>>> origin/main
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
