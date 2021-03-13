470d70c98c8d2aa4ca30cff11ba30af21192af3b
yuta-iiduka <ota12041204@gmail.com>
2021-03-13 13:07:16 +0000

    make-calendar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,36 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    @targeturl = params[:targeturl]
+    @events = current_user.events
+    @event = Event.new
+    @event.user_id = current_user.id
+  end
+  
+  def create
+    event = Event.create(event_params)
+    event.user_id = current_user.id
+    event.save
+    redirect_to user_events_path
+  end
+  
+  def show
+    @events = current_user.events
+    @event = Event.find(params[:id])
+  end
+  
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to user_events_path(current_user)
+  end
+  
+  def edit
+    @event = Event.find(params[:id])
+  end
+  
+  private
+  
+  def event_params
+    params.require(:event).permit(:title, :content, :start_time , :end_time , :tasklink)
   end
 end
