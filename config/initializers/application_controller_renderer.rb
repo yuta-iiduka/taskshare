44d5b5bcd47aaa1c023f8638f3b8634036b27475
yuta-iiduka <ota12041204@gmail.com>
2021-03-18 10:07:55 +0000

    change-layout318

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b20a6a3570ad8461a08623fc9f365a103900780a78299ec91fcf2854bbd7c359bbea9d7435161c149ae1c298b29483e85f9b4791f77cf3bdd9e937296fef2fa8'
